d5fba0abde4234db9f96e5ad1da63c4fb80a1629
Adenbroughton <aden.broughton@barclays.com>
2024-05-21 10:10:36 +0100

    part 2

--- src/app.js
+++ src/app.js
@@ -7,9 +7,17 @@ const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get("/musicians", async (request, respond) => {
+    const musician = await Musician.findAll ();
+    respond.json(musician);
+})
 
-
-
+app.get("/musicians/:id", async (request, response) => {
+    const number = request.params.id;
+    const musician = await Musician.findByPk(number);
+    response.json(musician);
+}
+)
 
 
 
